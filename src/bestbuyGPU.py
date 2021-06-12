318ab2b89b8f4bd84acdb14aef0d4a0d7a6d0947
zoeyleqa@gmail.com <18235400+zoeyleqa@users.noreply.github.com>
2021-06-12 02:03:47 -0400

    update 6/12/2021

--- src/bestbuyGPU.py
+++ src/bestbuyGPU.py
@@ -3,11 +3,12 @@ from dotenv import load_dotenv
 import requests
 import time
 # import io
-import datetime
+from datetime import datetime
 import webbrowser
 import smtplib
 import ssl
 import os
+# import io
 from GPUlist import gpu_links
 
 # Create .env file path.
@@ -19,7 +20,7 @@ load_dotenv(dotenv_path)
 config = {
     "port": 465,  # For SSL
     # port: 587 # For TLS
-    "receiver_email": "wanhdynamite@gmail.com", 
+    "receiver_email": "wanhdynamite@gmail.com",
     # "receiver_email": "tunguyen2705@gmail.com",
     "cc": "wanhdynamite@gmail.com",
     "headers": {"User-Agent": "Mozilla/5.0"},
@@ -28,7 +29,7 @@ config = {
 From: Best Buy Monitor Bot
 MIME-Version: 1.0
 Content-type: text/html
-Subject: Testing testing!!!
+Subject: BestBuy GPU available maybe!!!
 
 <h2>Sup sup,</h2>
 <p>Best Buy item below might be available:</p>
@@ -40,7 +41,7 @@ Subject: Testing testing!!!
 """
 }
 
-        
+
 def getSkuID(name):
     return gpu_links[name][gpu_links[name].rfind("=")+1:len(gpu_links[name])]
 
@@ -55,10 +56,10 @@ def check():
             # open link and email to notify
             if gpuToCheck[gpu_name]:
                 webbrowser.open(gpu_links[gpu_name])
-                print("-------------", datetime.datetime.now(),
+                print("-------------", datetime.now(),
                       "-------------", "\nIn stock: ", gpu_name, )
                 # signal()
-                email_me(gpu_name)
+                # email_me(gpu_name)
 
 
 def check_single(name):
@@ -74,18 +75,19 @@ def check_single(name):
         unavailableBtn = "<button class=\"btn btn-disabled btn-lg btn-block add-to-cart-button\" disabled=\"\" type=\"button\"" + \
             " data-sku-id=\"" + getSkuID(name) + \
             "\" style=\"padding:0 8px\">Unavailable Nearby</button>"
-        # debug log
-        if not source.__contains__(soldOutBtn) and not source.__contains__(comingSoonBtn) and not source.__contains__(unavailableBtn):
-            with io.open('src/log'+ '-' + datetime.datetime.now(), "w", encoding="utf-8") as f:
-                f.write(soldOutBtn+"\n")
-                f.write(comingSoonBtn+"\n")
-                f.write(unavailableBtn+"\n")
-                f.write("--------------------------------------------------------\n")
-                f.write(source)
+        # # debug log
+        # if not source.__contains__(soldOutBtn) and not source.__contains__(comingSoonBtn) and not source.__contains__(unavailableBtn):
+        #     now = datetime.now().strftime("%m-%d-%Y-%H%M%S")
+        #     with io.open('src/log'+ '-' + now +'.txt', "w", encoding="utf-8") as f:
+        #         f.write(soldOutBtn+"\n")
+        #         f.write(comingSoonBtn+"\n")
+        #         f.write(unavailableBtn+"\n")
+        #         f.write("--------------------------------------------------------\n")
+        #         f.write(source)
     except:
         print("check single failed ... moving on")
         return False
-        
+
     return not source.__contains__(soldOutBtn) and not source.__contains__(comingSoonBtn) and not source.__contains__(unavailableBtn)
 
 
@@ -96,7 +98,8 @@ def signal():
 
 
 def email_me(name):
-    msg = config["headerMsg"] + "<h3>" + name + "</h3><br><br>" + gpu_links[name] + "<br>" + config["footerMsg"]
+    msg = config["headerMsg"] + "<h3>" + name + "</h3><br><br>" + \
+        gpu_links[name] + "<br>" + config["footerMsg"]
     print('Sending email...')
     # with smtplib.SMTP("smtp.gmail.com", port) as server:
     try:
@@ -104,7 +107,8 @@ def email_me(name):
         # server.starttls()
         # server.ehlo()
         server.login(os.getenv('SENDER_EMAIL'), os.getenv('PASSWORD'))
-        server.sendmail(os.getenv('SENDER_EMAIL'), config["receiver_email"], msg)
+        server.sendmail(os.getenv('SENDER_EMAIL'),
+                        config["receiver_email"], msg)
         print('----> email sent!')
         server.close()
     except:
